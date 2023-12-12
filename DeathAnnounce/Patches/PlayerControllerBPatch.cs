485703ec93fc3007d10ab89aa3645773a8e06eb0
Julian <julian.w16@gmail.com>
2023-12-12 17:46:13 +0100

    We have made an announce mod...

---
+++ DeathAnnounce/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,23 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace DeathAnnounce.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class PlayerControllerBPatch
+    {
+        [HarmonyPatch("KillPlayer")]
+        [HarmonyPostfix]
+        private static void AnnounceDeath(PlayerControllerB __instance)
+        {
+            if (__instance.IsOwner && __instance.isPlayerDead)
+            {
+                string message = __instance.playerUsername + " has died to <color=#00ff00ff>" + __instance.causeOfDeath.ToString() + "</color>!";
+                // Make the message red.
+                message = "<color=#ff0000ff>" + message + "</color>";
+
+                HUDManager.Instance.AddTextToChatOnServer(message);
+            }
+        }
+    }
+}
---
+++ DeathAnnounce/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using DeathAnnounce.Patches;
+
+namespace DeathAnnounce
+{
+    [BepInPlugin("DeathAnnounce", "WolfenGamesMods", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("DeathAnnounce");
+
+        private static Plugin PluginInstance;
+
+        internal static ManualLogSource Log;
+
+        public static Plugin GetInstance()
+        {
+            return PluginInstance;
+        }
+
+        void Awake()
+        {
+            if (PluginInstance == null)
+            {
+                PluginInstance = this;
+            }
+
+            Log = BepInEx.Logging.Logger.CreateLogSource("DeathAnnounce");
+
+            Log.LogInfo("WolfenGamesMods <DeathAnnounce> loaded");
+
+            harmony.PatchAll(typeof(Plugin));
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+    }
+}
